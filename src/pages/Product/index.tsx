38d875453e79bdddeb1aeebf719ca469f7974c11
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-02-16 16:16:29 -0300

    feat: dynamic section

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
+import Section from '../../components/Section'
 
 const Product = () => {
   const { id } = useParams()
@@ -7,6 +8,32 @@ const Product = () => {
   return (
     <>
       <Hero />
+      <Section title="Sobre o jogo" background="black">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates,
+          autem magnam blanditiis optio, eius totam officiis labore ab earum
+          neque doloremque fugiat itaque veniam suscipit reprehenderit
+          aspernatur, assumenda asperiores dolorum. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Ab laboriosam quam vero sequi omnis
+          quaerat expedita debitis assumenda officia reprehenderit, quas illum
+          ratione voluptas? Corporis animi minima dolorum? Aperiam, praesentium.
+        </p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma:</b> PlayStation 5 <br />
+          <b>Desenvolvedor:</b> Avalanche Software <br />
+          <b>Editora:</b> Portkey Games, subsidiária da Warner Bros Interactive
+          Entertainment . <br />
+          <b>Idiomas:</b> O jogo oferece suporte a diversos idiomas, incluindo
+          inglês, espanhol, francês, alemão, italiano, português, entre outros.
+          As opções de áudio e legendas podem ser ajustadas nas configurações do
+          jogo.
+        </p>
+      </Section>
+      <Section title="Galeria" background="black">
+        <div>fotos</div>
+      </Section>
     </>
   )
 }
