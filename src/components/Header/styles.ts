793513ae6a2126e7878d6426e5e31f497a2701cb
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-03-05 13:25:15 -0300

    style: add responsive menu

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,37 +2,48 @@ import styled from 'styled-components'
 
 import { breakpoints, colors } from '../../styles'
 
+export const Links = styled.ul`
+  display: flex;
+  margin-left: 40px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-left: 0;
+    display: block;
+  }
+`
+
 export const HeaderBar = styled.header`
   background-color: ${colors.gray};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 
   a {
     color: ${colors.white};
     font-weight: bold;
   }
-
-  div {
-    display: flex;
-    align-items: center;
-  }
-
-  @media (max-width: ${breakpoints.tablet}) {
-    display: none;
-  }
 `
 
-export const Links = styled.ul`
-  display: flex;
-  margin-left: 40px;
+export const NavMobile = styled.nav`
+  display: none;
+
+  &.is-open {
+    display: block;
+  }
 `
 
 export const LinkIten = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-right: 0;
+
+    a {
+      display: block;
+      padding: 16px 0;
+      text-align: center;
+    }
+  }
 `
 
 export const CartButton = styled.a`
@@ -41,5 +52,45 @@ export const CartButton = styled.a`
 
   img {
     margin-left: 16px;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    span {
+      display: none;
+    }
+  }
+`
+
+export const Hamburguer = styled.div`
+  width: 32px;
+  span {
+    height: 2px;
+    display: block;
+    width: 100%;
+    background-color: ${colors.white};
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: ${breakpoints.tablet}) {
+    display: none;
+  }
+`
+
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    @media (max-width: ${breakpoints.tablet}) {
+      flex: 1;
+      justify-content: space-between;
+      ${Links} {
+        display: none;
+      }
+    }
   }
 `
